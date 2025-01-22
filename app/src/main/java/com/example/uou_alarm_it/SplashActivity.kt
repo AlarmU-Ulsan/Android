1204b775299388c2e9d7ae177c3927af1a75a922
uuon <uuon1029@mail.ulsan.ac.kr>
2025-01-22 14:33:11 +0900

    [Feat] Activity Binding ## Activity Binding - NoticeActivity.kt - SplashActivity.kt

--- app/src/main/java/com/example/uou_alarm_it/SplashActivity.kt
+++ app/src/main/java/com/example/uou_alarm_it/SplashActivity.kt
@@ -2,14 +2,14 @@ package com.example.uou_alarm_it
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.uou_alarm_it.databinding.ActivityNoticeBinding
+import com.example.uou_alarm_it.databinding.ActivitySplashBinding
 
-class NoticeActivity : AppCompatActivity() {
-    lateinit var binding : ActivityNoticeBinding
+class SplashActivity : AppCompatActivity() {
+    lateinit var binding : ActivitySplashBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityNoticeBinding.inflate(layoutInflater)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
     }
