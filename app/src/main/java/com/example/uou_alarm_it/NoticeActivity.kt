c6ef04ef7af25dccd9e4d71d5426d935ff31f2b9
uuon <uuon1029@mail.ulsan.ac.kr>
2025-01-22 15:07:16 +0900

    [Feat] RV ## RV connect - NoticeActivity.kt

--- app/src/main/java/com/example/uou_alarm_it/NoticeActivity.kt
+++ app/src/main/java/com/example/uou_alarm_it/NoticeActivity.kt
@@ -2,15 +2,24 @@ package com.example.uou_alarm_it
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.uou_alarm_it.databinding.ActivityNoticeBinding
 
 class NoticeActivity : AppCompatActivity() {
     lateinit var binding : ActivityNoticeBinding
+    private val noticeRVAdapter = NoticeRVAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityNoticeBinding.inflate(layoutInflater)
 
+        initRV()
+
         setContentView(binding.root)
+    }
+
+    private fun initRV() {
+        binding.noticeRv.adapter = noticeRVAdapter
+        binding.noticeRv.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
     }
 }
