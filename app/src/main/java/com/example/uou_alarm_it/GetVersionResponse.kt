061afa7645922138a84ad35cf3eaab89e4372229
2uon <it.wondong@gmail.com>
2025-03-23 17:14:50 +0900

    [API] getVersion api

---
+++ app/src/main/java/com/example/uou_alarm_it/GetVersionResponse.kt
@@ -0,0 +1,15 @@
+package com.example.uou_alarm_it
+
+data class GetVersionResponse(
+    val isSuccess: Boolean,
+    val code: String,
+    val message: String,
+    val result: Result
+) {
+    data class Result(
+        val id: Int,
+        val platform: String,
+        val latestVersion: String,
+        val minimumVersion: String
+    )
+}
--- app/src/main/java/com/example/uou_alarm_it/SplashActivity.kt
+++ app/src/main/java/com/example/uou_alarm_it/SplashActivity.kt
@@ -6,6 +6,9 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.uou_alarm_it.databinding.ActivitySplashBinding
 import com.google.firebase.BuildConfig
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SplashActivity : AppCompatActivity() {
     lateinit var binding : ActivitySplashBinding
@@ -19,19 +22,41 @@ class SplashActivity : AppCompatActivity() {
         val version = BuildConfig.VERSION_NAME
         binding.splashVersionTv.text = version.toString()
 
+        RetrofitClient.service.getVersion().enqueue(object : Callback<GetVersionResponse> {
+            override fun onResponse(
+                call: Call<GetVersionResponse>,
+                response: Response<GetVersionResponse>
+            ) {
+                if(response.code() == 200) {
+                    if (response.body()?.result?.latestVersion == version) {
+                        // 버전이 최신 버전일 때, 알림 클릭 or 메인 화면 이동
+                        intent?.extras?.let{
+                            link = it.getString("link") ?:""
+                        }
 
-        intent?.extras?.let{
-            link = it.getString("link") ?:""
-        }
+                        android.os.Handler().postDelayed({
+                            Log.d("splashFCM", link)
+                            val intent = Intent(this@SplashActivity, NoticeActivity::class.java).apply {
+                                putExtra("link", link)  // 알림에 포함된 데이터 전송
+                            }
+                            startActivity(intent)
+                            finish()
+                        },2000)
+                    } else {
+                        // 버전이 최신 버전이 아닐 때, 업데이트 다이얼로그 띄우기
 
-        android.os.Handler().postDelayed({
-            Log.d("splashFCM", link)
-            val intent = Intent(this, NoticeActivity::class.java).apply {
-                putExtra("link", link)  // 알림에 포함된 데이터 전송
+                    }
+                } else {
+                    // 네트워크 에러 다이얼 로그 표시하기
+                }
             }
-            startActivity(intent)
-            finish()
-        },2000)
+
+            override fun onFailure(call: Call<GetVersionResponse>, t: Throwable) {
+                Log.e("GetVersion", t.toString())
+                // 네트워크 에러 다이얼 로그 표시하기
+            }
+
+        })
     }
 
 
